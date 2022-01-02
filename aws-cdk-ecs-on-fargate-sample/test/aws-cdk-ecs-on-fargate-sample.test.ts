760c1319e2e372cfc4cbe90e20d03a56cfd997bc
Wakabayashi Ryo <wakabayashi@r-unit.co.jp>
2022-01-02 17:38:31 +0900

    first commit

---
+++ aws-cdk-ecs-on-fargate-sample/test/aws-cdk-ecs-on-fargate-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkEcsOnFargateSample from '../lib/aws-cdk-ecs-on-fargate-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-ecs-on-fargate-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkEcsOnFargateSample.AwsCdkEcsOnFargateSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
